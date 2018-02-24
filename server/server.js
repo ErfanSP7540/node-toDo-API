45784dc478b8c87846f2e5abc96c9487d9358814
erfan <erfan.seidipoor@ut.ac.ir>
2018-02-24 18:26:19 +0330

    deletebyId

--- server/server.js
+++ server/server.js
@@ -76,6 +76,60 @@ app.get('/users',(req,res)=>{
     .catch(  e=>{  console.log( 'error ' , e ) ;res.status(400).send(req.body)})
 })
 
+
+
+app.get('/user/:id', (req, res)=> 
+{
+   // res.send( req.params )
+
+    var {ObjectID} = require('mongodb')
+    if( !ObjectID.isValid(req.params.id) )
+    {
+        res.status(404).send();
+        return console.log(`Request ID: ${req.params.id}   >> NOT VALID`); 
+    }
+
+
+    userModel.findById(req.params.id)
+    .then ( doc=>{
+
+            if(!doc)
+                {   res.status(200).send(`Request ID: ${req.params.id} >> NOT FOUND`);
+                    return console.log(`Request ID: ${req.params.id} >> NOT FOUND`);
+                }
+            res.status(200).send(`Request ID: ${req.params.id} >> ${doc}`)    
+            return console.log(`Request ID: ${req.params.id} >> ${doc}`);
+    })
+    .catch( e  =>{   res.status(400).send(`Request ID: ${req.params.id} >> ERROR`);
+                     return console.log(`Request ID: ${req.params.id} >> ERROR`);    
+                  })
+})
+
+app.delete('/user/:id',(req,res)=>{
+    
+    var id = req.params.id;
+    var {ObjectID} =  require('mongodb');
+    if(!ObjectID.isValid(id)){
+            res.status(404).send(` Request ID : ${id} IS INVALID`)
+            return  console.log(` Request ID : ${id} IS INVALID`);
+        }
+
+    userModel.findOneAndRemove({_id:id})
+    .then( doc=>{
+        if(!doc){
+            res.status(404).send(` Request ID : ${id} Not Found`)
+            return  console.log(` Request ID : ${id} Not Found`);            
+        }
+        res.status(200).send(` Request ID : ${id} >>\n${doc}`)
+        return  console.log(` Request ID : ${id} >>\n${doc}`);    
+    })
+    .catch ( e=>{
+        res.status(404).send(` Request ID : ${id} ERROR `)
+        return  console.log(` Request ID : ${id} ERROR `);       
+    }) 
+
+})
+
 // app.use((req,res,next)=>{
 //     console.log('url    : ',req);
 //     console.log('method : ',req.method);
